3c5ca538ab3f19746249ce7d824a1873186618bd
hugorithm <hugofsilv@gmail.com>
2024-01-21 22:38:59 +0000

    Add replace current word keymap

--- nvim/lua/hugorithm/remap.lua
+++ nvim/lua/hugorithm/remap.lua
@@ -14,3 +14,5 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
