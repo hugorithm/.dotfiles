562812bcd36701315b3fff8b0830afe925db146f
hugorithm <hugofsilv@gmail.com>
2024-03-07 20:00:38 +0000

    Remove duplicates

--- .config/nvim/lua/hugorithm/set.lua
+++ .config/nvim/lua/hugorithm/set.lua
@@ -1,4 +1,7 @@
 vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = false
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -8,30 +11,25 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 10
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.opt.isfname:append '@-@'
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
-vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = false
+vim.opt.colorcolumn = '80'
 
-vim.opt.guicursor = ""
+vim.opt.guicursor = ''
 vim.opt.mouse = 'a'
 vim.opt.breakindent = true
 vim.opt.ignorecase = true
