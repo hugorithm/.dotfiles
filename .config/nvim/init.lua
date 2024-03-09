eeff34244134f661f4407a7ea00070a9210569e2
hugorithm <hugofsilv@gmail.com>
2024-03-09 16:20:32 +0000

    Remove gurvbox.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -341,7 +341,6 @@ require('lazy').setup({
 
   {
     -- 'rose-pine/neovim',
-    'ellisonleao/gruvbox.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
