06b44d296e7c914272269a9afbab468130c0589e
hugorithm <hugofsilv@gmail.com>
2024-03-09 17:28:35 +0000

    Add gruvbox-material

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -342,15 +342,11 @@ require('lazy').setup({
   {
     -- 'rose-pine/neovim',
     -- 'morhetz/gruvbox',
-    'gruvbox-material',
     -- 'rebelot/kanagawa.nvim',
+    'gruvbox-material',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-
       -- require('rose-pine').setup {
       --   styles = {
       --     bold = true,
