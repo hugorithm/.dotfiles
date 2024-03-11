2816e9127ac8e34e02b75ea5405988a47eb93094
hugorithm <hugofsilv@gmail.com>
2024-03-11 15:19:31 +0000

    Rename group

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -166,7 +166,7 @@ require('lazy').setup({
     },
     config = function()
       vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+        group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
         callback = function(event)
           local map = function(keys, func, desc)
             vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
